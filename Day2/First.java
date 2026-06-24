21f389ff202ce2c5604e06bcc4c5e94ba0be5d2f
Tejas-0570 <waydandetejas111@gmail.com>
2026-06-24 19:10:41 +0530

    Day 2 : Introduction to Java

---
+++ Day2/First.java
@@ -0,0 +1,11 @@
+package Day2;
+import java.io.*; // Package
+//Access Modifier | Pre-defined Keyword | ClassName
+public class First {
+    //Access Modifier | keyword | return type | main Method | Command Line Argument
+    public static void main(String[] args){
+
+        System.out.println("Keep Smiling..."); // Message
+        //Predefined class | printStream class ref | Method
+    }
+}
