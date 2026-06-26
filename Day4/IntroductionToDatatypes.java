f94ee36f076d940f237c0e55e7ad081876457071
Tejas-0570 <waydandetejas111@gmail.com>
2026-06-26 13:28:21 +0530

    Introduction to Datatypes

---
+++ Day4/IntroductionToDatatypes.java
@@ -0,0 +1,24 @@
+package Day4;
+public class IntroductionToDatatypes {
+    public static void main(String[] args){
+        // Datatypes
+        // 1. Primitive
+
+        boolean isBoolean = true;
+        byte Byte = 10;
+        short Short = 20;
+        int number = 100;
+        long contact = 100000;
+
+        float PI = 3.14F;
+        double d = 2345.234567;
+
+        char ch = 'A';
+
+
+        // 2. Non-primitive
+        String str = "Tejas";
+        String[] cars = {"BMW", "Audi", "Ford", "Mercedes"}; // String array
+        int[] marks = {90, 92, 85, 79}; // Integer array
+    }
+}
