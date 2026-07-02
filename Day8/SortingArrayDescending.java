0d27c43751b616ded6efc782f7582cd02dfe58c5
Tejas-0570 <waydandetejas111@gmail.com>
2026-07-02 14:08:40 +0530

    Sorting Array in Descending order

---
+++ Day8/SortingArrayDescending.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+class SortingArrayDescending{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of Array : ");
+        int size = sc.nextInt();
+        int[] array = new int[size];
+        for(int i = 0; i < size; i++){
+            System.out.print("Enter element at index "+i+" : ");
+            array[i] = sc.nextInt();
+        }
+
+        // 41 5 66 23 33
+
+        for(int i = 0; i < size; i++){
+            int temp = 0;
+            for(int j = i+1; j < size; j++){
+                if(array[i] < array[j]){
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+
+        System.out.println("\n------------------- Array Elements -------------------");
+        System.out.print("Array = { ");
+        for(int i = 0; i < size; i++){
+            System.out.print(array[i]+" ");
+        }
+        System.out.print("}");
+    }
+}
